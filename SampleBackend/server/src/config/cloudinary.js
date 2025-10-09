4e40d5ad77fa74ae5242f843fd32fb7dcb07b77c
arpit168 <arpitgupta0406@gmail.com>
2025-10-09 07:50:51 +0530

    Add Cloudinary configuration and update environment variables

---
+++ SampleBackend/server/src/config/cloudinary.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { v2 } from "cloudinary";
+
+v2.config({
+    CLOUD_NAME = dleozjmux,
+    API_KEY = 281615287443512,
+    API_SECRET = bzIoXW5RmdzR-H8wzblBOtL2EZE
+
+})
+export v2;
