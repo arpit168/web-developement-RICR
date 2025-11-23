4d049a2ca3357f92363bf8c599c906e0e8d1da29
arpit168 <arpitgupta0406@gmail.com>
2025-11-23 11:19:29 +0530

    calculator from html done

--- javaScript/Registration form/script.js
+++ javaScript/Registration form/script.js
@@ -1,3 +1,10 @@
 function submit() {
-    
+  let selectedBatchTiming = [];
+  document.querySelectorAll("input[name='batch']:checked" , "input[name='fullName']:checked " ,"input[name='email']:checked " ,"input[name='mobile']:checked ","input[name='dob']:checked ","input[name='qualification']:checked " ,"input[name='score']:checked ")
+  .forEach(() => {
+    selectedBatchTiming.push(Element.value);
+  });
+  console.log(selectedBatchTiming);
+
+  //   const selectedBatch = batch.forEach()
 }
