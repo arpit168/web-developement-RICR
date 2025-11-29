7798c1ce99e140728080a01fdba686e405b90e3c
arpit168 <arpitgupta0406@gmail.com>
2025-11-29 13:44:51 +0530

    forever

---
+++ e-commerce/frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Route, Routes, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import collection from "./pages/collection";
+import about from "./pages/about";
+import Contact from "./pages/Contact";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import PlaceOrder from "./pages/PlaceOrder";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<about />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/olace-order" element={<PlaceOrder />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
